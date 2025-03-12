8619a65669f73a795d7f2272b84e0ab2f4d8bfe7
pricemou <scameleons3@gamil.com>
2025-03-11 21:07:06 -0400

    Ajout de env

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use('/', indexRoutes);
 console.log(catMe());
 
 // Démarrer le serveur
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Serveur démarré sur http://localhost:${PORT}`);
 });
